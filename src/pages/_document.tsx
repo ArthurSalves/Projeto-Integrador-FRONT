8106c9cf736152728e99f0c9444028850031f5be
Arthur Scheffer Alves <128423597+ArthurSalves@users.noreply.github.com>
2023-06-28 15:15:52 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,19 @@
 import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
+interface Context extends NextPageContext {
+}
+
+
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: Context) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
+          enhanceApp: (App) => (props: any) =>
             sheet.collectStyles(<App {...props} />),
         })
 
